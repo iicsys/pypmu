16c66665591db6dfa2b5f003e74b08cabc0a85d6
Stevan Sandi <stevan.sandi@gmail.com>
2016-03-29 14:18:50 +0200

    fix reporting on benchPDC

--- tests/benchPDC.py
+++ tests/benchPDC.py
@@ -4,14 +4,13 @@ import time
 import os
 
 from synchrophasor.pdc import Pdc
-from synchrophasor.frame import FrameError
 from argparse import ArgumentParser
 from datetime import datetime
-
+from time import sleep
 
 argument_parser = ArgumentParser(description='benchPDC - will connect to given PDC'
                                              'Usage example: '
-                                             './behchPDC.py -i 511 -ip 127.0.0.1 -p 1995 -r 30')
+                                             './benchPDC.py -i 511 -ip 127.0.0.1 -p 1995 -r 30')
 
 argument_parser.add_argument('-i', '--id', help='PDC ID code.', default=7734, type=int)
 argument_parser.add_argument('-ip', '--ip', help='PMU IP.', required=True)
@@ -23,6 +22,15 @@ argument_parser.add_argument('-m', '--method', help='Transmission method TCP or 
 argument_parser.add_argument('-b', '--buffer', help='Transmission method buffer size.', default=2048, type=int)
 arguments = argument_parser.parse_args()
 
+# Receive ideal number of measurements in 60s.
+measurements2receive = 60 * arguments.data_rate
+timestamp = datetime.now().strftime('%Y%m%d_%H:%M:%S')
+errors = 0
+
+print("Running PDC(s) on {:d} separate thread(s) waiting for {:d}  measurements with expected"
+      " data rate of {:d} measurements per second.\n".format(arguments.jobs, measurements2receive, arguments.data_rate))
+sleep(2)
+
 pdc = Pdc(pdc_id=arguments.id, pmu_ip=arguments.ip, pmu_port=arguments.port,
           buffer_size=arguments.buffer, method=arguments.method)
 
@@ -30,11 +38,6 @@ pdc.run()  # Connect to PMU
 
 header = pdc.get_header()  # Get header message from PMU
 config = pdc.get_config()  # Get configuration from PMU
-
-# Receive ideal number of measurements in 60s.
-measurements2receive = 60 * arguments.data_rate
-timestamp = datetime.now().strftime('%Y%m%d_%H:%M:%S')
-errors = 0
 
 # Create result folder
 if not os.path.exists('results'):
@@ -47,20 +50,21 @@ start_time = stop_time = time.time()
 while measurements2receive > 0:
     try:
         data = pdc.get()  # Keep receiving data
-    except FrameError:
+
+        if measurements2receive == 1:
+            stop_time = time.time()
+
+        if not data:
+            pdc.quit()  # Close connection
+            break
+
+        measurements2receive -= 1
+    except:
+        print("Whooa! Something went wrong!")
         errors += 1
-
-    if measurements2receive == 1:
-        stop_time = time.time()
-
-    if not data:
-        pdc.quit()  # Close connection
-        break
-
-    measurements2receive -= 1
 
 # Write results to file:
 with open(log, 'w') as log_file:
     log_file.write("PDC ID: {:d}\n".format(arguments.id))
-    log_file.write("Result: {:f}\n".format(stop_time-start_time))
+    log_file.write("Result: {0:.4f}\n".format(stop_time-start_time))
     log_file.write("Errors: {:d}\n".format(errors))

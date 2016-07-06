84865f252741f6faceb7609eacfae7bb9e4fc16e
Stevan Sandi <stevan.sandi@itsystems.me>
2016-07-06 13:49:00 +0200

    changed to unstable version

--- apps/splytter.py
+++ apps/splytter.py
@@ -14,7 +14,7 @@ __author__ = "Stevan Sandi"
 __copyright__ = "Copyright (c) 2016, Tomo Popovic, Stevan Sandi, Bozo Krstajic"
 __credits__ = []
 __license__ = "BSD-3"
-__version__ = "1.0"
+__version__ = "0.1"
 
 argument_parser = ArgumentParser(description='splytter - replicate data stream to many PDCs. '
                                              'Usage example: '
--- synchrophasor/utils.py
+++ synchrophasor/utils.py
@@ -93,7 +93,7 @@ __author__ = "Stevan Sandi"
 __copyright__ = "Copyright (c) 2016, Tomo Popovic, Stevan Sandi, Bozo Krstajic"
 __credits__ = []
 __license__ = "BSD-3"
-__version__ = "1.0"
+__version__ = "0.1"
 
 
 def list2bytes(int_list, byte_size):

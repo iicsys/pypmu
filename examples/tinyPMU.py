2059c3d9f551f00b7136dc3f9b4ed8c88b711ebf
Stevan Sandi <stevan.sandi@itsystems.me>
2017-02-22 14:26:11 +0100

    remove message

--- examples/tinyPMU.py
+++ examples/tinyPMU.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     pmu.set_configuration()  # This will load default PMU configuration specified in IEEE C37.118.2 - Annex D (Table D.2)
     pmu.set_header()  # This will load default header message "Hello I'm tinyPMU!"
-    print("Here we are!")
+
     pmu.run()  # PMU starts listening for incoming connections
 
     while True:

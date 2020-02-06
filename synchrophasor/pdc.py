36225f0cda1074bb1874f62d61746e5fa74536a9
Yuri Poledna <ypoledna@gmail.com>
2020-02-06 17:06:28 -0300

    Fixing name

--- synchrophasor/pdc.py
+++ synchrophasor/pdc.py
@@ -14,7 +14,7 @@ __version__ = "1.0.0-alpha"
 
 ###############################################################
 
-#  UDP connection was implemented by Yury Poledna under the supervision of 
+#  UDP connection was implemented by Yuri Poledna under the supervision of 
 
 #  Prof. Eduardo Parente in R&D project supported by Brazilian electric utility 
 
--- synchrophasor/splitter.py
+++ synchrophasor/splitter.py
@@ -11,7 +11,7 @@ __version__ = "1.0.0-aplha"
 
 ###############################################################
 
-#  UDP connection was implemented by Yury Poledna under the supervision of 
+#  UDP connection was implemented by Yuri Poledna under the supervision of 
 
 #   Prof. Eduardo Parente in R&D project supported by Brazilian electric utility 
 
